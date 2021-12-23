fb98d334bc84de90d1c992fd0a8c08ec9c772e87
Wlandry <landrywabo8@gmail.com>
2021-12-23 11:45:49 +0100

    Création de la page d'acceuil et les formulaires de connection et d'inscription d'un utlisateur

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login(): Response
+    {
+        return $this->render('security/login.html.twig');
+    }
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout(): Response
+    {
+       
+    }
+    /**
+     * @Route("/register", name="security_register")
+     */
+    public function register(): Response
+    {
+        return $this->render('security/register.html.twig');
+    }
+}
